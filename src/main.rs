205ed1a45403c54a1852930e7ede7597ea9d5cb6
Matt Archer <matthewarcherr@gmail.com>
2024-03-18 16:14:29 +0000

    Tidied up code

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,19 @@
-use std::io::{stdin,stdout,Write};
 use console::Term;
 
 fn main() {
     let term = Term::stdout();
     term.set_title("Vimail");
-    println!("[35mMagenta[0m");
-    println!("[107m    [2J");
-    
+    println!("[107m[2J");
 
-    
-    
-    
+    let (y, x) = term.size();
+
+    term.move_cursor_to(((x/2)-4).into(), (y/2).into()).unwrap();
+    println!("[30mTEST");
 
     loop {
-        match term.read_char() {
-            Ok(v) => {
-                if v == 'q' {
-                    println!("[0m    [2J");
-                    break;
-                }
-            },
-            Err(_) => {}
+        if term.read_char().unwrap() == 'q' {
+            println!("[0m    [2J");
+            break;
         }
     }
 
